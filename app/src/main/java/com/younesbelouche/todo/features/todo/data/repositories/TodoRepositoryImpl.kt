ece40d3ab9816f10875be44792a76b7bf9bb092f
Dom Broks <tinoutech@gmail.com>
2026-02-04 12:38:48 +0100

    Refactor repo for maintaining reactivity

--- app/src/main/java/com/younesbelouche/todo/features/todo/data/repositories/TodoRepositoryImpl.kt
+++ app/src/main/java/com/younesbelouche/todo/features/todo/data/repositories/TodoRepositoryImpl.kt
@@ -4,27 +4,30 @@ import com.younesbelouche.todo.core.util.Result
 import com.younesbelouche.todo.features.todo.data.datasources.InMemoryTodoDataSource
 import com.younesbelouche.todo.features.todo.data.mappers.toDomain
 import com.younesbelouche.todo.features.todo.data.mappers.toDto
+import com.younesbelouche.todo.features.todo.data.models.TaskDto
 import com.younesbelouche.todo.features.todo.domain.entities.Task
 import com.younesbelouche.todo.features.todo.domain.repositories.TodoRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class TodoRepositoryImpl(
     private val dataSource: InMemoryTodoDataSource
 ) : TodoRepository {
 
-    override fun getTasks(): Flow<Result<List<Task>>> = flow {
-
-        emit(Result.Loading)
-
-        val tasks = dataSource.getTasks().map { it.toDomain() }
-        emit(Result.Success(tasks))
-
-
-    }.catch { e ->
-        emit(Result.Error(e))
-    }
+    override fun getTasks(): Flow<Result<List<Task>>> =
+        dataSource.tasks
+            .map<List<TaskDto>, Result<List<Task>>> { dtos ->
+                Result.Success(dtos.map { it.toDomain() })
+            }
+            .onStart {
+                emit(Result.Loading)
+            }
+            .catch { e ->
+                emit(Result.Error(e))
+            }
 
     override suspend fun addTask(task: Task): Result<Unit> {
         return try {
